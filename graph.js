69f17e190b4cadd2f3a3501e15784a5601bf0536
Andrés C. Viesca Ruiz <viestat@icloud.com>
2016-03-10 17:01:43 -0800

    Add wieght to graph

--- graph.js
+++ graph.js
@@ -1,53 +1,50 @@
-  var Graph = function(){
-    this.nodes = [];
-  }
+var Graph = function(){
+  this.nodes = [];
+}
 
-  Graph.prototype.addNode = function(val){
-    var exists = false;
-    this.nodes.forEach(function(el){
-      if(el[0] === val){
-        exists = true;
-      }
-    });
-    if(!exists){
-      this.nodes.push([val,[]]);
+Graph.prototype.addNode = function(val){
+  var exists = false;
+  this.nodes.forEach(function(el){
+    if(el[0] === val){
+      exists = true;
     }
+  });
+  if(!exists){
+    this.nodes.push([val,[]]);
   }
+}
 
 
-  Graph.prototype.removeNode = function(val){
-    var j;
-    var k;
-    this.nodes.forEach(function(el, i){
-      if(el[0] === val){
-        j = i;
-      } else {
-        el[1].forEach(function(edge, ind){
-          if(edge === val){
-            k = ind;
-          }
-        });
-        el[1].splice(k,1);
-      }
-    });
-    this.nodes.splice(j,1);
-  }
-
-  Graph.prototype.addEdge = function(node1, node2, directed){
-    directed = directed || false;
-    this.nodes.forEach(function(el){
-      if(el[0] === node1){
-        if(!!el[1].indexOf(node2)){
-          el[1].push(node2);
+Graph.prototype.removeNode = function(val){
+  var j;
+  var k;
+  this.nodes.forEach(function(el, i){
+    if(el[0] === val){
+      j = i;
+    } else {
+      el[1].forEach(function(edge, ind){
+        if(edge[0] === val){
+          k = ind;
         }
+      });
+      el[1].splice(k,1);
+    }
+  });
+  this.nodes.splice(j,1);
+}
+
+Graph.prototype.addEdge = function(node1, node2, w, directed){
+  directed = directed || false;
+  w = w || 1;
+  this.nodes.forEach(function(el){
+    if(el[0] === node1){
+      el[1].push([node2, w]);
+    }
+    if(!directed){
+      if(el[0] === node2){
+        el[1].push([node1, w]);
       }
-      if(directed){
-        if(el[0] === node2){
-          if(!!el[1].indexOf(node1)){
-            el[1].push(node1);
-          }
-        }
-      }
-    });
-  }
-//[val,[edges...]]
+    }
+  });
+}
+//[val,[[edge, w]...]]
